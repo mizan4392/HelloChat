3ac83b2e119798c9039a77579d19852c45ceebd8
mizan4392 <mizanur15-8164@diu.edu.bd>
2023-01-06 20:33:55 +0600

    HC-navBar

---
+++ client/tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+    screen: {
+      tab: "960px",
+    },
+  },
+  plugins: [],
+  mode: "jit",
+};
