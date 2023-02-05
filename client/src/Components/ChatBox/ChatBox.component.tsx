b72af05aa7b1cd1925a0b764b7c2875c497e2d2e
mizan4392 <mizanur15-8164@diu.edu.bd>
2023-02-05 23:46:44 +0600

    HC-chat-online-ui

--- client/src/Components/ChatBox/ChatBox.component.tsx
+++ client/src/Components/ChatBox/ChatBox.component.tsx
@@ -3,8 +3,8 @@ import Message from "../Message/Message.component";
 
 export default function ChatBox() {
   return (
-    <div className="wrapper">
-      <div className="boxTop">
+    <div className="wrapper flex flex-col ">
+      <div className="boxTop h-full overflow-y-scroll px-[10px]">
         <Message isOwnMessage={true} />
         <Message />
         <Message isOwnMessage={true} />
@@ -12,7 +12,18 @@ export default function ChatBox() {
         <Message isOwnMessage={true} />
         <Message />
       </div>
-      <div className="boxBottom"></div>
+      <div className="boxBottom mt-[5px] flex items-center justify-between ">
+        <textarea
+          className="w-[80%] h-[90px] p-[10px]"
+          placeholder="Write something"
+        ></textarea>
+        <button
+          className="w-[70px] h-[40px] cursor-pointer text-white"
+          style={{ border: "none", borderRadius: "5px", background: "teal" }}
+        >
+          Send
+        </button>
+      </div>
     </div>
   );
 }
